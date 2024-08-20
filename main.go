83f302581cbb740424456ba397f970e0ff449230
kitsada8621 <kitsada8621@gmail.com>
2024-08-20 10:46:09 +0700

    - set cors allow method and whitelist

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
@@ -27,16 +28,23 @@ func main() {
 
 	r := gin.Default()
 
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	}))
+
+	r.Use(gin.Recovery())
+
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("eqfield", utils.CustomEqField)
 	}
 
 	if err := database.EnsureSeederRoleData(); err != nil {
-		panic("Err seeder role: " + err.Error())
+		panic(err.Error())
 	}
 
 	if err := database.EnsureSeederAdminData(); err != nil {
-		panic("Err seeder account: " + err.Error())
+		panic(err.Error())
 	}
 
 	routes.InitRoute(r)
